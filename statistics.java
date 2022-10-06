38c4350db44bedf144841327a177d40cb362d418
SameerC1 <100710894+SameerC1@users.noreply.github.com>
2022-10-06 17:18:31 -0400

    Added the possible interface
    I know it's not large but this project is really small and we are probably just gonna use an imported function for all of the calculations so it doesen't matter.

---
+++ statistics.java
@@ -0,0 +1,9 @@
+//INTERFACE FOR THE CLASSES OF THE STATISTICS PROJECT
+//BY SAMEER CHOWDHURY
+
+interface statistics
+  {  
+    //function to retreive the list
+    int[] sort(int list[]);
+    int[] calculations(int list[]);
+  }
