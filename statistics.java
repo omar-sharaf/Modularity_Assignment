59f6f9b620557747dcaa10f852752e551e808e12
SameerC1 <100710894+SameerC1@users.noreply.github.com>
2022-10-09 12:26:44 -0400

    Deleted this interface
    Because we are supposed to do everything in one class, therefore this interface is pointless.

--- statistics.java
+++
@@ -1,9 +0,0 @@
-//INTERFACE FOR THE CLASSES OF THE STATISTICS PROJECT
-//BY SAMEER CHOWDHURY
-
-interface statistics
-  {  
-    //function to retreive the list
-    int[] sort(int list[]);
-    int[] calculations(int list[]);
-  }
